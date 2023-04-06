90d917621d8d4dfdc99b7dd73c7ae812ff823a1b
JitHoon <101972330+JitHoon@users.noreply.github.com>
2023-04-06 19:33:17 +0900

    basic syntex of java-day1

---
+++ section3/multiplication-table.java
@@ -0,0 +1,59 @@
+/* 1. Operator
+
++ - * / % (//, ** 없음 ㅋㅋ)
+리터럴 : 데이터(값) 그 자체를 뜻한다. 즉, 변수에 넣는 변하지 않는 데이터를 의미
+PRECEDENCE (우선 순위): * / % > + - 
+
+*/
+ 
+/* 2. System.out.println()
+
+class : System
+parameter : out
+method : println
+
+System.out.println("Hello \"World")
+Hello "World
+
+System.out.println("Hello \\World")
+Hello \World
+
+System.out.println("Hello\nWorld")
+System.out.println("Hello\tWorld") 
+*/
+
+/* 3. Math. methods
+
+Math.random()
+Math.min(1,0)
+*/
+
+/* 4. jshell 여러줄 사용 
+
+System.
+   ...> out.
+   ...> println(
+   ...> "hello") 
+
+혹은 ; 사용
+*/
+
+/* 5. System.out.printf().println()
+
+System.out.printf("%d * %d = %d", 5, 2, 5*2)
+5 * 2 = 10$22 ==> java.io.PrintStream@50040f0c
+
+System.out.printf("%d * %d = %d", 5, 2, 5*2).println()
+5 * 2 = 10
+
+%d %f %s
+*/
+
+/* 6. variable
+
+java는 유형 중심의 언어이다.
+
+int, float, 
+int number = 15
+int number2 = 25
+ */
